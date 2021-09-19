810f695a561c0471e3c94d8f7cb6cf829fe6b63c
hwan0303 <asok369258@naver.com>
2021-09-19 21:37:58 +0900

    베르트랑 공준

---
+++ baekjoon/수학/베르트랑 공준.py
@@ -0,0 +1,20 @@
+num = []
+n = int(input())
+while(n != 0):
+    num.append(n)
+    n = int(input())
+
+for i in num:
+    count = 0
+    arr = [True] * (2*i+1)
+    for j in range(2,int((2*i)**0.5)+1):
+        if arr[j]:
+            for k in range(j*j,2*i+1,j):
+                arr[k] = False
+
+    for j in range(i+1,2*i+1):
+        if arr[j]:
+            count += 1
+
+    print(count)
+    
