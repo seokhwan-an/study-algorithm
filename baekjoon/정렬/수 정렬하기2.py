f9d387a5e5289aeea2870cd91ca67e081c41b324
hwan0303 <asok369258@naver.com>
2021-09-22 22:08:32 +0900

    수 정렬하기2.py 추가

---
+++ baekjoon/정렬/수 정렬하기2.py
@@ -0,0 +1,29 @@
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+
+    merged_arr = []
+    l= h =0
+    while l < len(low_arr) and h <len(high_arr):
+        if low_arr[l] < high_arr[h]:
+            merged_arr.append(low_arr)
+            l += 1
+        else:
+            merged_arr.append(high_arr[h])
+            h += 1
+    merged_arr += low_arr[l:]
+    merged_arr += high_arr[h:]
+    return merged_arr
+
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+a = merge_sort(arr)
+for i in a:
+    print(i)
