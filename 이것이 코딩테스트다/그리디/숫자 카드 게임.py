80446e11e586d15c8ac71e6333e0164ab1e9c8e7
hwan0303 <asok369258@naver.com>
2021-10-11 23:48:58 +0900

    숫자 카드 게임.py

---
+++ 이것이 코딩테스트다/그리디/숫자 카드 게임.py
@@ -0,0 +1,9 @@
+N, M = map(int,input().split())
+data = []
+for i in range(N):
+    data.append(list(map(int,input().split())))
+big = min(data[0])
+for i in range(1,N):
+    if min(data[i]) > big:
+        big = min(data[i])
+print(big)
