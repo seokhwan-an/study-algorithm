bc645610d728e91db15ea9551e1142e5a750466a
hwan0303 <asok369258@naver.com>
2021-10-16 17:40:14 +0900

    Add 기능개발.py

---
+++ programers/스택,큐/기능개발.py
@@ -0,0 +1,27 @@
+def solution(progresses, speeds):
+    a = progresses
+    b = speeds
+    cnt = 0
+    answer = []
+    while True:
+        if len(a) == 0:
+            break
+        else:
+            for i in range(len(a)):
+                a[i] += b[i]
+            if a[0] < 100:
+                continue
+            while a[0] >= 100:
+                cnt += 1
+                a.pop(0)
+                b.pop(0)
+                if len(a) == 0:
+                    break
+            answer.append(cnt)
+            cnt = 0
+                
+    return answer
+
+progresses = [93,30,55]
+speeds = [1,30,5]
+print(solution(progresses, speeds))
