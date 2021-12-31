a42270a5247e202975d6c29c183b39962dca72a3
hwan0303 <asok369258@naver.com>
2022-01-01 08:12:06 +0900

    [MOD] 큰 수의 법칙.py

--- 이것이 코딩테스트다/그리디/큰 수의 법칙.py
+++ 이것이 코딩테스트다/그리디/큰 수의 법칙.py
@@ -1,26 +1,5 @@
-# 내가 푼 풀이
-N, M, K = map(int, input().split())
-arr = list(map(int,input().split()))
-arr.sort(reverse=True)
-first = arr[0]
-second = arr[1]
-term = first*K + second
-answer = (M//(K+1))*term + (M%(K+1))*first
-print(answer)
-
-# 책에서 설명해주는 간략하게 푸는 법(시간복잡도 낮게)
-N, M, K = map(int, input().split())
-arr = list(map(int,input().split()))
-arr.sort(reverse=True)
-first = arr[0]
-second = arr[1]
-cnt = 1
-answer = 0
-while(cnt <= M):
-    if cnt % (K+1) == 0:
-        answer += second
-        cnt += 1
-    else:
-        answer += first
-        cnt += 1
+N,M,K = map(int, input().split())
+num_list = list(map(int,input().split()))
+num_list.sort(reverse=True)
+answer = num_list[0] * (M//K) * K + num_list[1] * (M%K)
 print(answer)
