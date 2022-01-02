e06d682c6c8deb4a4af4bf54ce13fe8caf0b4fbe
hwan0303 <asok369258@naver.com>
2022-01-02 20:06:33 +0900

    [MOD] 숫자 카드 게입.py

--- 이것이 코딩테스트다/그리디/숫자 카드 게임.py
+++ 이것이 코딩테스트다/그리디/숫자 카드 게임.py
@@ -8,13 +8,3 @@ for i in range(1,N):
     if min(data[i]) > big:
         big = min(data[i])
 print(big)
-
-# 책에서 알려주는 풀이
-n, m = map(int,input().split())
-result = 0
-for i in range(n):
-    data = list(map(int,input().split()))
-    min_value = min(data)
-    result = max(result,min_value)
-
-print(result)
