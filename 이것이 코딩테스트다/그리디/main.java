a6d9385c8ee44281e54c19d709e958ee6fb3e9f9
seokhwan-an <asok369258@naver.com>
2022-02-14 16:39:56 +0900

    [ADD]BOJ_2110.java

---
+++ 이것이 코딩테스트다/그리디/main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+    }
+}
