4b9eaac2e640aad03c43c1b407079f2240bcd172
seokhwan-an <asok369258@naver.com>
2022-02-17 17:24:36 +0900

    [ADD]BOJ_1654.java

---
+++ baekjoon/분류/이진 탐색/BOJ_1654.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_1654 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] arr = new long[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        long s = 1, e = arr[N-1];
+        long answer = 0;
+        while(s <= e){
+            long mid = (s + e)/2;
+            int cnt = 0;
+            for(int i = 0; i < N; i++){
+                if(arr[i] >= mid){
+                    cnt += arr[i] / mid;
+                }
+            }
+            if(cnt >= M){
+                s = mid + 1;
+                answer = Math.max(answer, mid);
+            }
+            else{
+                e = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
