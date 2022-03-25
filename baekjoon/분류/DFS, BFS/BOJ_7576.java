9b7a92a7673b07b70656b8d8479ba3e352a2a3f1
seokhwan-an <asok369258@naver.com>
2022-03-25 14:22:03 +0900

    [UPDATE] BOJ_7576.java(진행중)

--- baekjoon/분류/DFS, BFS/BOJ_7576.java
+++ baekjoon/분류/DFS, BFS/BOJ_7576.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class BOJ_7576 {
@@ -12,10 +14,14 @@ public class BOJ_7576 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] map = new int[N][M];
+        ArrayList<int[]> check = new ArrayList<>();
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 1){
+                    check.add(new int[]{i, j});
+                }
             }
         }
     }
