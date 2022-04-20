f1b3a39de9d231f4415649f51f53d8ec74a8cd4a
SeokHwan An <asok369258@naver.com>
2022-04-20 13:18:36 +0900

    [UPDATE]BOJ_2579.java(해결)

--- baekjoon/분류/다이나믹 프로그래밍/BOJ_2579.java
+++ baekjoon/분류/다이나믹 프로그래밍/BOJ_2579.java
@@ -6,39 +6,18 @@ public class BOJ_2579 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] step = new int[N+2];
-        for(int i = 2; i < N+2; i++){
+        int[] step = new int[N+1];
+        for(int i = 1; i < N+1; i++){
             step[i] = Integer.parseInt(br.readLine());
         }
-        int[][] answer = new int[N+2][2];
-        answer[0][0] = answer[1][0] = answer[0][1] = answer[1][1] = 0;
-        for(int i = 2; i < N+2; i++){
-            if(i == N+1){ // 3번 조건
-                if(answer[i-1][1] == 2){//2번 제약조건 3번 연속 불가;
-                    answer[i][0] = answer[i-2][0] + step[i];
-                    answer[i][1] = 1;
-                }
-                else{
-                    answer[i][0] = Math.max(answer[i-1][0],answer[i-2][0]) + step[i];
-                }
-            }
-            else{
-                if(answer[i-1][1] == 2){//2번 제약조건 3번 연속 불가;
-                    answer[i][0] = answer[i-2][0] + step[i];
-                    answer[i][1] = 1;
-                }
-                else{
-                    if(answer[i-1][0] > answer[i-2][0]) { // 1번 조건
-                        answer[i][0] = answer[i-1][0] + step[i];
-                        answer[i][1] = answer[i-1][1] + 1;
-                    }
-                    else{
-                        answer[i][0] = answer[i-2][0] + step[i];
-                        answer[i][1] = 1;
-                    }
-                }
-            }
+        int[] answer = new int[N+1];
+        answer[1] = step[1];
+        if(N >= 2){
+            answer[2] = step[1] + step[2];
         }
-        System.out.println(answer[N+1][0]);
+        for(int i = 3; i < N+1; i++){
+            answer[i] = Math.max(answer[i-2],answer[i-3]+step[i-1]) + step[i];
+        }
+        System.out.println(answer[N]);
     }
 }
