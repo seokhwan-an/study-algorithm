17667d891fddd7c1624524d2edb33bcae1f589ec
SeokHwan An <asok369258@naver.com>
2022-04-25 21:31:03 +0900

    [Update] BOJ_11725.java(해결)

--- baekjoon/분류/DFS, BFS/BOJ_11725.java
+++ baekjoon/분류/DFS, BFS/BOJ_11725.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_11725 {
@@ -9,35 +11,34 @@ public class BOJ_11725 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        int[][] graph = new int[N][2];
-        int[] arr = new int[N+1];
-        for(int i = 0; i < N+1; i++){
-            arr[i] = -1;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+        int[] arr1 = new int[N+1];
+        boolean[] arr2 = new boolean[N+1];
+        for(int i = 1; i <= N+1; i++){
+            graph.add(new ArrayList<>());
         }
         for(int i = 0; i < N-1; i++){
             st = new StringTokenizer(br.readLine());
-            graph[i][0] = Integer.parseInt(st.nextToken());
-            graph[i][1] = Integer.parseInt(st.nextToken());
-            if(arr[graph[i][0]] == -1 || arr[graph[i][1]] == -1){
-                if(graph[i][1] == 1){
-                    arr[graph[i][0]] = 1;
-                }
-                if(graph[i][0] == 1){
-                    arr[graph[i][1]] = 1;
-                }
-                if(graph[i][1] != 1 && graph[i][0] != 1 && arr[graph[i][1]] == -1){
-                    arr[graph[i][1]] = graph[i][0];
-                }
-            }
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            graph.get(s).add(e);
+            graph.get(e).add(s);
         }
-        for(int i = 0; i < N-1; i++){
-            if(arr[graph[i][0]] == -1){
-                arr[graph[i][0]] = graph[i][1];
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.offer(1);
+        while(!q.isEmpty()){
+            int y = q.poll();
+            arr2[y] = true;
+            for(int num : graph.get(y)){
+                if(arr2[num] == false){
+                    arr2[num] = true;
+                    arr1[num] = y;
+                    q.offer(num);
+                }
             }
         }
         for(int i = 2; i < N+1; i++){
-            System.out.println(arr[i]);
+            System.out.println(arr1[i]);
         }
-
     }
 }
