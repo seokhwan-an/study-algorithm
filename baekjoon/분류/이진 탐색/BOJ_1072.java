d2df376a3e873398f4f814cc159ec10822af79cc
SeokHwan An <asok369258@naver.com>
2022-08-10 10:18:27 +0900

    [Update]BOJ_1072.java(오류해결)

--- baekjoon/분류/이진 탐색/BOJ_1072.java
+++ baekjoon/분류/이진 탐색/BOJ_1072.java
@@ -8,24 +8,30 @@ public class BOJ_1072 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
+        long x = Integer.parseInt(st.nextToken());
+        long y = Integer.parseInt(st.nextToken());
+        int z = win_rate(x, y);
 
-        int left = 0, right = x, mid = 0;
-        long next = 0;
+        long left = 0, right = x, mid = 0;
+        int next = 0;
         if (z >= 99) {
             System.out.println(-1);
         } else {
             while (left <= right) {
                 mid = (left + right) / 2;
-                next = (y + mid) * 100 / (x + mid);
+                next = win_rate(x + mid, y + mid);
                 if (next > z) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             }
-            System.out.println(mid);
+            System.out.println(left);
         }
+    }
+
+    public static int win_rate(long x, long y) {
+        int win_rate = (int)(y * 100 / x);
+        return win_rate;
     }
 }
