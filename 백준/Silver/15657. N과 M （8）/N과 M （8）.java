492c0e2ca1595595c9aaac83a790d8c4a35c0dfa
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2023-12-21 20:49:23 +0900

    [Silver III] Title: N과 M (8), Time: 180 ms, Memory: 18332 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] numbers;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final String[] inputs = br.readLine().split(" ");
+
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
+        numbers = new int[N];
+        arr = new int[M];
+
+        final String[] numberInputs = br.readLine().split(" ");
+
+        for (int i = 0; i < numberInputs.length; i++) {
+            numbers[i] = Integer.parseInt(numberInputs[i]);
+        }
+        Arrays.sort(numbers);
+
+        solution(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void solution(int k, int pre) {
+        if (k == M) {
+            for (int number : arr) {
+                sb.append(number).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= pre) {
+                arr[k] = numbers[i];
+                solution(k + 1, numbers[i]);
+            }
+        }
+    }
+}
