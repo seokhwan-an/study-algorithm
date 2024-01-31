04cc91ba37320c206167c875000fa974d493c26a
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2024-01-31 10:14:03 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 0; i < progresses.length; i++) {
+            int cnt = 1;
+            int progress = progresses[i];
+            int speed = speeds[i];
+            
+            int lastTime = getLastTime(progress, speed);
+            
+            int nextCnt = 0;
+            for (int j = i + 1; j < progresses.length; j++) {
+                if (progresses[j] + lastTime * speeds[j] >= 100) {
+                    nextCnt += 1;
+                    cnt += 1;
+                    i += 1;
+                } else {
+                    break;
+                }
+            }
+            
+            answer.add(cnt);
+        }
+        
+        
+        return changeToArray(answer);
+    }
+    
+    private int getLastTime(int progress, int speed) {
+        int lastWork = 100 - progress;
+        if (lastWork % speed == 0) {
+            return lastWork / speed;
+        }
+        return lastWork / speed + 1;
+    }
+    
+    private int[] changeToArray(List<Integer> answer) {
+        int[] array = new int[answer.size()];
+        
+        for (int i = 0; i < answer.size(); i++) {    
+            array[i] = answer.get(i);
+        }
+        
+        return array;
+
+    }
+}
