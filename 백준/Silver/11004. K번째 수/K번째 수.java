640f71d09c6d4f0c3b53d8b6a74c1ebc8cc2ba7f
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2024-02-23 20:36:16 +0900

    [Silver V] Title: K번째 수, Time: 3492 ms, Memory: 460236 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int cnt = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            num.add(Integer.parseInt(st.nextToken()));
+        }
+        num.sort(Comparator.naturalOrder());
+        System.out.println(num.get(cnt-1));
+    }
+}
