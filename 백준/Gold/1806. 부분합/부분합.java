895cee6535544827e433419a75837344772f5417
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2024-03-15 22:14:04 +0900

    [Gold IV] Title: 부분합, Time: 288 ms, Memory: 22440 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        long sum = 0;
+        while (true) {
+            if (sum >= S) {
+                answer = Math.min(answer, end - start);
+                sum -= arr[start++];
+            } else if (end >= N) {
+                break;
+            } else if (sum < S) {
+                sum += arr[end++];
+            }
+        }
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(answer);
+        }
+    }
+}
