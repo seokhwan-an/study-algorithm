7a22bbfcca6be4e5f70fc47bdfe86b16222a428b
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2024-10-31 09:48:19 +0900

    [Silver II] Title: 좌표 압축, Time: 2272 ms, Memory: 221120 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -4,33 +4,49 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        Map<Integer, Integer> arr1 = new HashMap<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
-        int[] check = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            int num = Integer.parseInt(st.nextToken());
-            arr2.add(num);
-            check[i] = num;
+        String[] input = br.readLine().split(" ");
+
+        int[] numbers = new int[N];
+        Set<Integer> check = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(input[i]);
+            numbers[i] = num;
+            check.add(num);
         }
-        Collections.sort(arr2);
-        for(int i = 0; i < arr2.size(); i++){
-            if(arr1.keySet().contains(arr2.get(i))){
-                continue;
-            }
-            else{
-                arr1.put(arr2.get(i),cnt++);
+        List<Integer> sorted = new ArrayList<>(check);
+        Collections.sort(sorted);
+        int[] answer = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int target = numbers[i];
+
+            int start = 0;
+            int end = sorted.size() - 1;
+            while(start <= end) {
+                int mid = (start + end) / 2;
+                int find = sorted.get(mid);
+
+                if (target == find) {
+                    answer[i] = mid;
+                    break;
+                } else if (target < find) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
+
         StringBuilder sb = new StringBuilder();
-        for(int key : check) {
-            sb.append(arr1.get(key)).append(" ");
+        for (int i = 0; i < N; i++) {
+            sb.append(answer[i]).append(" ");
         }
-        System.out.println(sb);
+
+        System.out.println(sb.toString());
     }
 }
