4b7e470178c3db9a2bedfabd2ea524b79e2b6dda
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2025-02-28 13:49:36 +0900

    [Silver II] Title: N과 M (9), Time: 88 ms, Memory: 37024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+
+nums = list(map(int, input().split()))
+nums.sort()
+answer = []
+# 중복 방지
+visited = [False] * n
+check = set()
+# 수열이 앞선 수보다 증가 한 숫자만 넣는다
+def solution(count, max, find):
+    if count == max:
+        if find not in check:
+            check.add(find)
+            answer.append(find)
+        return
+
+    for i in range(len(nums)):
+        if not visited[i]:
+            visited[i] = True
+            solution(count + 1, m, find + str(nums[i]) + " ")
+            visited[i] = False
+solution(0, m, "")
+
+print("\n".join(answer))
