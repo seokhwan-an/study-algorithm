1169bc77257299dd8b5bffb2889124684b51cccf
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2025-03-04 16:07:43 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677?language=python3) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 2.56 ms
+메모리: 10.3 MB, 시간: 1.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 20:53:17
+2025년 03월 04일 16:07:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -0,0 +1,58 @@
+# 유사한 기사를 묶기 
+# 자카드 유사도 : 집합간의 유사도를 검사하는 방법 
+# 두 집합의 교집합의 크기를 합집합의 크기로 나눈 값
+# 두 집합 모두 공집합일 경우 자카드 유사도는 1로 정의
+
+# 중복을 허용하는 다중집합에 대해서 자카드 유사도를 구하기
+# 문자열을 두글자씩 자른 배열 구하기 -> 영어만 포함된 경우 모두 소문자로 변경해서
+# 교집합 구하기, 합집합 구하기
+# 교집합의 길이를 합집합으로 나누고 65536 곱하기
+def solution(str1, str2):
+    answer = 0
+    num = 65536
+    
+    str1 = makeArray(str1)
+    str2 = makeArray(str2)
+    
+    if len(str1) == 0 and len(str2) == 0:
+        return num
+    
+    
+    dic1 = makeDic(str1)
+    dic2 = makeDic(str2)
+    
+    sameUnit = 0
+    allUnit = 0
+    
+    for unit in set(str1):
+        if unit in dic1 and unit in dic2:
+            sameUnit += min(dic1[unit], dic2[unit])
+            allUnit += max(dic1[unit], dic2[unit])
+        elif unit in dic1:
+            allUnit += dic1[unit]
+    for unit in set(str2):
+        if unit in dic2 and unit not in dic1:
+            allUnit += dic2[unit]
+    
+    jakade = sameUnit / allUnit
+    answer = int(num * jakade)
+    return answer
+
+def makeArray(string):
+    splitString = []
+    
+    for i in range(len(string) - 1):
+        splited = string[i:i + 2]
+        if splited.isalpha():
+            splitString.append(splited.lower())
+    return splitString
+
+def makeDic(array):
+    dic = {}
+    for unit in array:
+        if unit in dic:
+            dic[unit] += 1
+        else:
+            dic[unit] = 1
+    
+    return dic
