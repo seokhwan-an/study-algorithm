e0d5fce53ca0954ff0bca299a0ae172685cd38bf
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2025-03-06 14:49:38 +0900

    [Gold V] Title: 수 고르기, Time: 188 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -0,0 +1,26 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+arr = []
+for i in range(n):
+    arr.append(int(sys.stdin.readline()))
+
+arr.sort()
+answer = float("inf")
+start, end = 0, 0
+diff = 0
+while start <= end and end < len(arr):
+    if diff > m:
+        answer = min(answer, diff)
+        start += 1
+        diff = arr[end] - arr[start]
+    else:
+        if diff == m:
+            answer = m
+            break
+        end += 1
+        if end >= len(arr):
+            break
+
+        diff = arr[end] - arr[start]
+print(answer)
