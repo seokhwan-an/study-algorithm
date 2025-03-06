5358c0544170e69f993c1109405ccfa4572b385f
SeokHwan An <70303795+seokhwan-an@users.noreply.github.com>
2025-03-06 14:51:23 +0900

    [Gold V] Title: 수 고르기, Time: 188 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -10,14 +10,11 @@ answer = float("inf")
 start, end = 0, 0
 diff = 0
 while start <= end and end < len(arr):
-    if diff > m:
+    if diff >= m:
         answer = min(answer, diff)
         start += 1
         diff = arr[end] - arr[start]
     else:
-        if diff == m:
-            answer = m
-            break
         end += 1
         if end >= len(arr):
             break
